435f2ebf4e4da78bbf52e78e6cee2621120b73fa
DidacClaria <dclariah99@gmail.com>
2020-04-24 12:49:07 +0200

    user basic structure

--- Proyecto-COVID/user.c
+++ Proyecto-COVID/user.c
@@ -7,8 +7,20 @@ int pid;
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
+	int i;
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
-
-  while(1) { }
+	pid=fork();
+	while(1) { 
+		if (pid==0){
+			i=getpid();
+			itoa(i,buff);
+			write(1, buff, strlen(buff));
+		}
+		else {
+			i=getpid();
+			itoa(i,buff);
+			write(1, buff, strlen(buff));
+		}
+	}
 }
